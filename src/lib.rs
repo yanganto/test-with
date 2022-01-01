ab6c5bf73fe4bd2f8561c15979370738877fa12b
Antonio Yang <yanganto@gmail.com>
2022-01-01 10:02:20 +0800

    Readme

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,24 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, ItemFn};
 
+/// Run test case when the environment variable is set.
+/// ```
+/// #[cfg(test)]
+/// mod tests {
+///
+///     // PWD environment variable exists
+///     #[test_with::env(PWD)]
+///     fn test_works() {
+///         assert!(true);
+///     }
+///
+///     // NOTHING environment variable does not exist
+///     #[test_with::env(NOTHING)]
+///     fn test_ignored() {
+///         panic!("should be ignored")
+///     }
+/// }
+/// ```
 #[proc_macro_attribute]
 pub fn env(attr: TokenStream, stream: TokenStream) -> TokenStream {
     let input = parse_macro_input!(stream as ItemFn);
