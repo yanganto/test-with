c446d42bdc55f47fbab1a062714a0994893bd83c
Antonio Yang <yanganto@gmail.com>
2022-01-03 12:57:24 +0800

    Implement `file`, `path` (#12)

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::{fs::metadata, path::Path};
+
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, ItemFn};
@@ -47,6 +49,130 @@ pub fn env(attr: TokenStream, stream: TokenStream) -> TokenStream {
         }
     }
     return if all_var_exist {
+        quote! {
+            #(#attrs)*
+            #[test]
+            #vis #sig #block
+        }
+        .into()
+    } else {
+        quote! {
+           #(#attrs)*
+           #[test]
+           #[ignore = #ignore_msg ]
+           #vis #sig #block
+        }
+        .into()
+    };
+}
+
+/// Run test case when the file exist.
+/// ```
+/// #[cfg(test)]
+/// mod tests {
+///
+///     // hostname exists
+///     #[test_with::file(/etc/hostname)]
+///     fn test_works() {
+///         assert!(true);
+///     }
+///
+///     // nothing file does not exist
+///     #[test_with::file(/etc/nothing)]
+///     fn test_ignored() {
+///         panic!("should be ignored")
+///     }
+///
+///     // hostname and hosts exist
+///     #[test_with::file(/etc/hostname, /etc/hosts)]
+///     fn test_works_too() {
+///         assert!(true);
+///     }
+/// }
+/// ```
+#[proc_macro_attribute]
+pub fn file(attr: TokenStream, stream: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(stream as ItemFn);
+    let ItemFn {
+        attrs,
+        vis,
+        sig,
+        block,
+    } = input;
+    let attr_str = attr.to_string().replace(" ", "");
+    let files: Vec<&str> = attr_str.split(',').collect();
+    let mut all_file_exist = true;
+    let mut ignore_msg = "because following file not found:".to_string();
+    for file in files.iter() {
+        if !Path::new(file).is_file() {
+            all_file_exist = false;
+            ignore_msg.push('\n');
+            ignore_msg.push_str(file);
+        }
+    }
+    return if all_file_exist {
+        quote! {
+            #(#attrs)*
+            #[test]
+            #vis #sig #block
+        }
+        .into()
+    } else {
+        quote! {
+           #(#attrs)*
+           #[test]
+           #[ignore = #ignore_msg ]
+           #vis #sig #block
+        }
+        .into()
+    };
+}
+
+/// Run test case when the path(file or folder) exist.
+/// ```
+/// #[cfg(test)]
+/// mod tests {
+///
+///     // etc exists
+///     #[test_with::path(/etc)]
+///     fn test_works() {
+///         assert!(true);
+///     }
+///
+///     // nothing does not exist
+///     #[test_with::path(/nothing)]
+///     fn test_ignored() {
+///         panic!("should be ignored")
+///     }
+///
+///     // etc and tmp exist
+///     #[test_with::path(/etc, /tmp)]
+///     fn test_works_too() {
+///         assert!(true);
+///     }
+/// }
+/// ```
+#[proc_macro_attribute]
+pub fn path(attr: TokenStream, stream: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(stream as ItemFn);
+    let ItemFn {
+        attrs,
+        vis,
+        sig,
+        block,
+    } = input;
+    let attr_str = attr.to_string().replace(" ", "");
+    let paths: Vec<&str> = attr_str.split(',').collect();
+    let mut all_path_exist = true;
+    let mut ignore_msg = "because following path not found:".to_string();
+    for path in paths.iter() {
+        if metadata(path).is_err() {
+            all_path_exist = false;
+            ignore_msg.push('\n');
+            ignore_msg.push_str(path);
+        }
+    }
+    return if all_path_exist {
         quote! {
             #(#attrs)*
             #[test]
