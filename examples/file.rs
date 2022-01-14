245b33b85a3fb1b3f889d1adcab07777043170c3
Antonio Yang <yanganto@gmail.com>
2022-01-14 13:23:50 +0800

    Check test attribute and add it if not exist (#19)

--- examples/file.rs
+++ examples/file.rs
@@ -11,54 +11,63 @@ mod tests {
 
     // nothing file does not exist
     #[test_with::file(/etc/nothing)]
+    #[test]
     fn test_ignored() {
         panic!("should be ignored")
     }
 
     // hostname and hosts exist
     #[test_with::file(/etc/hostname, /etc/hosts)]
+    #[test]
     fn test_works_too() {
         assert!(true);
     }
 
     // nothing file does not exist
     #[test_with::file(/etc/hostname, /etc/nothing)]
+    #[test]
     fn test_ignored_too() {
         panic!("should be ignored")
     }
 
     // etc exists, but not file
     #[test_with::file(/etc)]
+    #[test]
     fn test_ignored_for_file() {
         panic!("should be ignored")
     }
 
     // hostname exists
     #[test_with::path(/etc/hostname)]
+    #[test]
     fn test_works_for_file() {
         assert!(true);
     }
 
     // etc exists
     #[test_with::path(/etc)]
+    #[test]
     fn test_works_for_path() {
         assert!(true);
     }
 
     // nothing does not exist
     #[test_with::path(/nothing)]
+    #[test]
     fn test_ignored_for_path() {
         panic!("should be ignored")
     }
 
     // etc and tmp exist
     #[test_with::path(/etc, /tmp)]
+    #[test]
     fn test_works_for_paths_too() {
         assert!(true);
     }
 
     // nothing does not exist
     #[test_with::file(/etc, /nothing)]
+    #[test]
     fn test_ignored_for_paths_too() {
         panic!("should be ignored")
     }
--- examples/net.rs
+++ examples/net.rs
@@ -3,11 +3,13 @@ fn main() {}
 #[cfg(test)]
 mod http_tests {
     #[test_with::http(httpbin.org)]
+    #[test]
     fn test_works() {
         assert!(true);
     }
 
     #[test_with::https(not.exist.com)]
+    #[test]
     fn test_ignored() {
         panic!("should be ignored")
     }
@@ -16,11 +18,13 @@ mod http_tests {
 #[cfg(test)]
 mod https_tests {
     #[test_with::https(www.rust-lang.org)]
+    #[test]
     fn test_works() {
         assert!(true);
     }
 
     #[test_with::https(not.exist.com)]
+    #[test]
     fn test_ignored() {
         panic!("should be ignored")
     }
@@ -29,11 +33,13 @@ mod https_tests {
 #[cfg(test)]
 mod icmp_tests {
     #[test_with::icmp(127.0.0.1)]
+    #[test]
     fn test_works() {
         assert!(true);
     }
 
     #[test_with::icmp(193.194.195.196)]
+    #[test]
     fn test_ignored() {
         panic!("should be ignored")
     }
@@ -42,11 +48,13 @@ mod icmp_tests {
 #[cfg(test)]
 mod tcp_tests {
     #[test_with::tcp(8.8.8.8:53)]
+    #[test]
     fn test_works() {
         assert!(true);
     }
 
     #[test_with::tcp(193.194.195.196)]
+    #[test]
     fn test_ignored() {
         panic!("should be ignored")
     }
