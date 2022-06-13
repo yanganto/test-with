4c26ecef27617e7e3a7b5013275db11353ccc733
Antonio Yang <yanganto@gmail.com>
2022-06-13 22:38:44 +0800

    Use customized dr

--- flake.nix
+++ flake.nix
@@ -4,9 +4,10 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
+    dependency-refresh.url = "github:yanganto/dependency-refresh";
   };
 
-  outputs = { self, rust-overlay, nixpkgs, flake-utils }:
+  outputs = { self, rust-overlay, nixpkgs, flake-utils, dependency-refresh }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ (import rust-overlay) ];
@@ -14,12 +15,13 @@
           inherit system overlays;
         };
         rust = pkgs.rust-bin.nightly."2022-03-30".default;
+        dr = dependency-refresh.defaultPackage.${system};
+
         publishScript = pkgs.writeShellScriptBin "crate-publish" ''
           cargo login $1
           cargo publish
         '';
         updateDependencyScript = pkgs.writeShellScriptBin "update-dependency" ''
-          cargo install dependency-refresh
           dr ./Cargo.toml
           if [ -f "Cargo.toml.old" ]; then
             rm Cargo.toml.old
@@ -42,6 +44,7 @@
             openssl
             pkg-config
 
+            dr
             publishScript
             featureTestScript
             updateDependencyScript
