c2a7c743bb986e2563308d5b8ec8d127a9144668
Antonio Yang <yanganto@gmail.com>
2025-03-29 14:12:49 +0800

    update locks

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
         pkgs = import nixpkgs {
           inherit system overlays;
         };
-        rust = pkgs.rust-bin.stable."1.77.0".default;
+        rust = pkgs.rust-bin.stable."1.77.2".default;
         dr = dependency-refresh.defaultPackage.${system};
 
         publishScript = pkgs.writeShellScriptBin "crate-publish" ''
