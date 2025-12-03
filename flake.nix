825f80f9cc08ce9bdd908567ee60273d361d77ad
Antonio Yang <yanganto@gmail.com>
2025-12-03 11:19:54 +0800

    update locks

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11-small";
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
     dependency-refresh = {
