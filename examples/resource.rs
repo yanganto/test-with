3a13043e8c004493387559c0b08f47b173ca77da
Antonio Yang <yanganto@gmail.com>
2026-01-28 21:24:39 +0800

    update test cases (#127)

--- examples/resource.rs
+++ examples/resource.rs
@@ -14,13 +14,13 @@ mod tests {
         panic!("should be ignored")
     }
 
-    #[test_with::cpu_core(32)]
+    #[test_with::cpu_core(64)]
     #[test]
     fn cpu_core_test_ignored() {
         panic!("should be ignored")
     }
 
-    #[test_with::phy_core(32)]
+    #[test_with::phy_core(64)]
     #[test]
     fn physical_cpu_core_test_ignored() {
         panic!("should be ignored")
